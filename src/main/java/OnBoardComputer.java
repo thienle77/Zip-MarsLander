3e2e9002feb23747d60452b38eac89570f01a609
thienle77 <tle9677@gmail.com>
2022-11-30 21:34:12 -0500

    tried something / landed

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -3,7 +3,10 @@ public class OnBoardComputer implements BurnStream {
     @Override
     public int getNextBurn(DescentEvent status) {
         int burn = 0;
-
+//        int v = status.getVelocity();
+//        int a = status.getAltitude();
+//
+//        burn = (v * v) / (2 * a);
         if (status.Altitude > 1) {
             burn = (status.Velocity * status.Velocity) / (2 / status.Altitude) + 100;
 
